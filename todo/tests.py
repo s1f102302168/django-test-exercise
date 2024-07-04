1734a9ed7747da157a4985f3f2c37375c9731139
1f10230216 <s1f102302168@iniad.org>
2024-07-04 11:28:48 +0900

    コード修正2

--- todo/tests.py
+++ todo/tests.py
@@ -38,7 +38,6 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
-
     def test_is_overdue_past(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
@@ -56,7 +55,6 @@ class TaskModelTestCase(TestCase):
         self.assertFalse(task.is_overdue(current))
 
 
-
 class TodoViewTestCase (TestCase):
     def test_index_get(self):
         client = Client()
@@ -64,7 +62,7 @@ class TodoViewTestCase (TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(len(response.context ['tasks']), 0)
+        self.assertEqual(len(response.context['tasks']), 0)
 
     def test_index_post(self):
         client = Client()
@@ -76,9 +74,9 @@ class TodoViewTestCase (TestCase):
         self.assertEqual(len(response.context['tasks']), 1)
 
     def test_index_get_order_post(self):
-        task1 = Task(title= 'task1', due_at=timezone.make_aware(datetime (2024, 7, 1)))
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
-        task2 = Task(title= 'task2', due_at=timezone.make_aware(datetime (2024, 8, 1)))
+        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
         task2.save()
         client = Client()
         response = client.get('/?order=post')
@@ -89,9 +87,9 @@ class TodoViewTestCase (TestCase):
         self.assertEqual(response.context['tasks'][1], task1)
 
     def test_index_get_order_due(self):
-        task1 = Task(title= 'task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
-        task2 = Task(title= 'task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
+        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
         task2.save()
         client = Client()
         response = client.get('/?order=due')
